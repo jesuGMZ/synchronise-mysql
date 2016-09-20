92934eff70f96fc965724f8f77fdd1f94ff89755
jesuGMZ <gmz2006@gmail.com>
2016-09-20 14:23:17 +0200

    Cut long lines

--- synchronise-mysql.bat
+++ synchronise-mysql.bat
@@ -31,20 +31,31 @@ rem set credentials
 mysql_config_editor.exe set --user=%mysql_origin_user% --password
 
 rem import a dump from the origin database
-echo "Importing a dump from %mysql_origin_database% database from host %mysql_origin_database%..."
-mysqldump.exe -h %mysql_origin_host% -P %mysql_origin_port% %mysql_origin_database% > %temporal_dump_file_name%
+echo "Importing a dump from %mysql_origin_database% database from host" ^
+    "%mysql_origin_database%..."
+mysqldump.exe -h %mysql_origin_host% ^
+    -P %mysql_origin_port% ^
+    %mysql_origin_database% > %temporal_dump_file_name%
 
 rem delete current destination database
-echo "Deleting database %mysql_origin_database% in host %mysql_destination_host%..."
-mysqladmin.exe -h %mysql_destination_host% -P %mysql_destination_port% drop -f %mysql_destination_database%
+echo "Deleting database %mysql_origin_database% in host" ^
+    "%mysql_destination_host%..."
+mysqladmin.exe -h %mysql_destination_host% ^
+    -P %mysql_destination_port% ^
+    drop -f %mysql_destination_database%
 
 rem create an empty destination database
-echo "Creating an empty database %mysql_destination_database% in host %mysql_destination_host%..."
-mysqladmin.exe -h %mysql_destination_host% -P %mysql_destination_port% create %mysql_destination_database%
+echo "Creating an empty database %mysql_destination_database% in host" ^
+    "%mysql_destination_host%..."
+mysqladmin.exe -h %mysql_destination_host% ^
+    -P %mysql_destination_port% ^
+    create %mysql_destination_database%
 
 rem import the dump into the destination database
 echo "Importing dump into %mysql_destination_host%..."
-mysql.exe -h %mysql_destination_host% -P %mysql_destination_port% %mysql_origin_database% < %temporal_dump_file_name%
+mysql.exe -h %mysql_destination_host% ^
+    -P %mysql_destination_port% ^
+    %mysql_origin_database% < %temporal_dump_file_name%
 
 rem delete the temporal dump file
 echo "Deleting temporal dump file..."
